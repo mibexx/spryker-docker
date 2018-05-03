1f979371cc8bb1850e3c53fae46cd24706873f55
Mike Bertram <contact@mibexx.de>
2018-05-03 10:47:52 +0200

    * Docker cp

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,6 @@ sh ./scripts/configure_rabbitmq.sh
 
 PROXY_CONTAINER=$(docker ps --filter name="proxy" -q | awk '{ print $1 }')
 
-docker cp $PROXY_CONTAINER:/root/.ssh/id_rsa ./ssh_key
+docker cp "$PROXY_CONTAINER:/root/.ssh/id_rsa" ./ssh_key
 
 ansible-playbook -i ./inventory playbooks/deploy.yaml --key-file "./ssh_key"
