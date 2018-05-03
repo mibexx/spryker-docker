6cc1b9b6b4e76cb0c08d020dde36e41d49c31afc
Mike Bertram <contact@mibexx.de>
2018-05-03 12:46:24 +0200

    + New structure
    proxy = admin
    admin = php

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,8 +2,6 @@
 
 sh ./scripts/configure_rabbitmq.sh
 
-PROXY_CONTAINER=$(docker ps --filter name="spryker_proxy" -q | awk '{ print $1 }')
-
+PROXY_CONTAINER=$(docker ps --filter name="spryker_admin" -q | awk '{ print $1 }')
 docker cp "$PROXY_CONTAINER:/root/.ssh/id_rsa" ./ssh_key
-
 ansible-playbook -i ./inventory playbooks/deploy.yaml --key-file "./ssh_key"
--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@
 DOCKER_VENDOR="mibexx"
 IMAGE_PREFIX="spryker"
 
-echo "Build proxy image"
-docker build ./proxy -t $DOCKER_VENDOR/$IMAGE_PREFIX-proxy
-
 echo "Build admin image"
+docker build ./php -t $DOCKER_VENDOR/$IMAGE_PREFIX-php
+
+echo "Build proxy image"
 docker build ./admin -t $DOCKER_VENDOR/$IMAGE_PREFIX-admin
 
 echo "Build jenkins image"
